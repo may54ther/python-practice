a8c443a3015d9b1947c0683f9a3b440f2870f388
kimahyeon <kim.ahyeon@ahope.co.kr>
2024-11-18 15:38:07 +0900

    Chapter02: add templates

--- 241118_flask_mega_tutorial/microblog/app/routes.py
+++ 241118_flask_mega_tutorial/microblog/app/routes.py
@@ -1,15 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Tabber'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    user = {'username': 'tabber'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, post=posts)
