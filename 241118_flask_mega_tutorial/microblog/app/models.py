89508da885aa40e363b9fd0623098a875017e86d
kimahyeon <kim.ahyeon@ahope.co.kr>
2024-11-19 12:44:22 +0900

    Chapter05: add password hash

--- 241118_flask_mega_tutorial/microblog/app/models.py
+++ 241118_flask_mega_tutorial/microblog/app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
@@ -11,11 +12,16 @@ class User(db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(
-        back_populates='author')
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
