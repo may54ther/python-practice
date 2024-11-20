b6bb99725ed8b1deedd771d29a5ab5b852e49db8
kimahyeon <kim.ahyeon@ahope.co.kr>
2024-11-20 15:37:43 +0900

    Chapter05: add register form

--- 241118_flask_mega_tutorial/microblog/app/routes.py
+++ 241118_flask_mega_tutorial/microblog/app/routes.py
@@ -4,7 +4,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app import app, db
 from app.models import User
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 
 
 @app.route("/")
@@ -42,3 +42,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratualtions, you are now a registered user!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
