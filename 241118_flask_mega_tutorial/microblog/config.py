60618d082368ad89bbfc2ba8da82104f8afeec93
kimahyeon <kim.ahyeon@ahope.co.kr>
2024-11-29 15:48:44 +0900

    Chapter08: add prev, next

--- 241118_flask_mega_tutorial/microblog/config.py
+++ 241118_flask_mega_tutorial/microblog/config.py
@@ -14,4 +14,4 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     ADMINS = ["kim.ahyeon@ahope.co.kr"]
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
