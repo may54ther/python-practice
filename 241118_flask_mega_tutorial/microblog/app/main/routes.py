fb0251e92cbea8a00ee67bd937a2182e4d9efe60
kimahyeon <kim.ahyeon@ahope.co.kr>
2024-12-09 14:42:56 +0900

    Chapter21: user notification

--- 241118_flask_mega_tutorial/microblog/app/main/routes.py
+++ 241118_flask_mega_tutorial/microblog/app/main/routes.py
@@ -8,8 +8,8 @@ from langdetect import LangDetectException, detect
 
 from app import db
 from app.main import bp
-from app.main.forms import EditProfileForm, EmptyForm, PostForm, SearchForm
-from app.models import Post, User
+from app.main.forms import EditProfileForm, EmptyForm, MessageForm, PostForm, SearchForm
+from app.models import Message, Notification, Post, User
 from app.translate import translate
 
 
@@ -103,13 +103,15 @@ def user(username):
         prev_url=prev_url,
         form=form,
     )
-    
-@bp.route('/user/<username>/popup')
+
+
+@bp.route("/user/<username>/popup")
 @login_required
 def user_popup(username):
     user = db.first_or_404(sa.select(User).where(User.username == username))
     form = EmptyForm()
-    return render_template('user_popup.html', user=user, form=form)
+    return render_template("user_popup.html", user=user, form=form)
+
 
 @bp.route("/edit_profile", methods=["GET", "POST"])
 @login_required
@@ -202,3 +204,58 @@ def search():
         next_url=next_url,
         prev_url=prev_url,
     )
+
+
+@bp.route("/send_message/<recipient>", methods=["GET", "POST"])
+@login_required
+def send_message(recipient):
+    user = db.first_or_404(sa.select(User).where(User.username == recipient))
+    form = MessageForm()
+    if form.validate_on_submit():
+        msg = Message(author=current_user, recipient=user, body=form.message.data)
+        db.session.add(msg)
+        user.add_notification("unread_message_count", user.unread_message_count())
+        db.session.commit()
+        flash(_("Your message has been sent."))
+        return redirect(url_for("main.user", username=recipient))
+    return render_template(
+        "send_message.html", title=_("Send Message"), form=form, recipient=recipient
+    )
+
+
+@bp.route("/messages")
+@login_required
+def messages():
+    current_user.last_message_read_time = datetime.now(timezone.utc)
+    current_user.add_notification("unread_message_count", 0)
+    db.session.commit()
+    page = request.args.get("page", 1, type=int)
+    query = current_user.messages_received.select().order_by(Message.timestamp.desc())
+    messages = db.paginate(
+        query, page=page, per_page=current_app.config["POSTS_PER_PAGE"], error_out=False
+    )
+    next_url = (
+        url_for("main.messages", page=messages.next_num) if messages.has_next else None
+    )
+    prev_url = (
+        url_for("main.messages", page=messages.prev_num) if messages.has_prev else None
+    )
+    return render_template(
+        "messages.html", messages=messages.items, next_url=next_url, prev_url=prev_url
+    )
+
+
+@bp.route("/notifications")
+@login_required
+def notifications():
+    since = request.args.get("since", 0.0, type=float)
+    query = (
+        current_user.notifications.select()
+        .where(Notification.timestamp > since)
+        .order_by(Notification.timestamp.asc())
+    )
+    notifications = db.session.scalars(query)
+    return [
+        {"name": n.name, "data": n.get_data(), "timestamp": n.timestamp}
+        for n in notifications
+    ]
